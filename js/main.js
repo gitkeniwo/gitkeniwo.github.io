5190e8b1157a12b70506e7f6af66fe7f5d516914
gitkeniwo <kennethwwwww@gmail.com>
2025-11-17 06:36:45 +0100

    Site updated: 2025-11-17 06:36:45

--- js/main.js
+++ js/main.js
@@ -21,68 +21,31 @@ $(document).ready(function() {
 
 
   /**
-   * Controls the different versions of  the menu in blog post articles 
+   * Controls the TOC terminal box in blog post articles
    * for Desktop, tablet and mobile.
    */
   if ($(".post").length) {
-    var menu = $("#menu");
-    var nav = $("#menu > #nav");
     var menuIcon = $("#menu-icon, #menu-icon-tablet");
+    var tocBox = $("#toc-terminal-box");
+    var tocCloseBtn = $("#toc-close-btn");
 
     /**
-     * Display the menu on hi-res laptops and desktops.
-     */
-    if ($(document).width() >= 1440) {
-      menu.css("visibility", "visible");
-      menuIcon.addClass("active");
-    }
-
-    /**
-     * Display the menu if the menu icon is clicked.
+     * Display the TOC box if the menu icon is clicked.
      */
     menuIcon.click(function() {
-      if (menu.css("visibility") === "hidden") {
-        menu.css("visibility", "visible");
-        menuIcon.addClass("active");
-      } else {
-        menu.css("visibility", "hidden");
-        menuIcon.removeClass("active");
-      }
+      tocBox.toggleClass("active");
+      menuIcon.toggleClass("active");
       return false;
     });
 
     /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
+     * Close TOC box when close button is clicked.
      */
-    if (menu.length) {
-
-      /// 隐藏导航栏 hide nav when have toc
-      if ($(".toc-item").length) {
-        nav.hide();
-      } else {
-
-      $(window).on("scroll", function() {
-        var topDistance = menu.offset().top;
-
-        // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 150) {
-          nav.show();
-        } else if (nav.is(":visible") && topDistance > 50) {
-          nav.hide();
-        }
-
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
-        }
-        }); 
-      } 
-    }
+    tocCloseBtn.click(function() {
+      tocBox.removeClass("active");
+      menuIcon.removeClass("active");
+      return false;
+    });
 
     
     
